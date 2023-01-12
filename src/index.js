ce515361138a3db81d066515ec60a03b1ca5227a
bricobc <barajas.c.bruno.n@gmail.com>
2023-01-11 23:20:13 -0600

    Use webpack in everything

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import './style.css'
 const fondo = document.querySelector('.fondo');
 
 const divCard = document.createElement('div');
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWepackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    resolve: {
+        extensions: ['.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ],
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWepackPlugin({
+            inject: true,
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin(),
+    ]
+}
